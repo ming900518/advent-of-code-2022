6bf3d27c84e3f72d9f03bcbbfba9dc845f1ddd62
Ming Chang <mail@mingchang.tw>
2023-07-29 19:37:16 +0800

    scala: upload code. (Day4)

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -3,5 +3,9 @@
   aoc.day1.part2(aoc.day1.input);
   aoc.day2.part1(aoc.day2.input);
   aoc.day2.part2(aoc.day2.input);
+  aoc.day3.part1(aoc.day3.input);
+  aoc.day3.part2(aoc.day3.input);
+  aoc.day4.part1(aoc.day4.input);
+  aoc.day4.part2(aoc.day4.input);
 }
 
